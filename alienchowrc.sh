706f622f4952b16e319fee7bcbb3cbc43d0e26c7
Lian Yuanlin <lian@timespacemagic.com>
2015-10-13 23:57:57 +0800

    Added rc file.

---
+++ alienchowrc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# ======= #
+# Hotkeys #
+# ======= #
+alias ll='ls -al'
+
+# ============ #
+# PROMPT TWEAK #
+# ============ #
+get_git_branch() {
+	BRANCH=$(git branch 2> /dev/null | grep "^* .\+$" | sed "s/^* //")
+	OUTPUT=
+	if [[ -n "$BRANCH" ]]; then OUTPUT="$BRANCH"; fi
+	if [[ -n "$OUTPUT" ]]; then echo "$OUTPUT"; fi
+}
+get_git_changes() {
+	CHANGES=$(git status 2> /dev/null | grep -e "^Changes not staged for commit:\|^Untracked files")
+	if [[ -n "$CHANGES" ]]; then echo "*"; fi
+}
+export PS1="[\[\e[31m\]\u\[\e[m\]@\[\e[1;31m\]\h\[\e[m\]:\W]\[\e[1;32m\](\$(get_git_branch)\[\e[1;37m\]\$(get_git_changes)\[\e[1;32m\])\[\e[m\]$ "
